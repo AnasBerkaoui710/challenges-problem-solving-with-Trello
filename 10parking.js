2710a85f7fb2d86787df8e17fbab25f60fe7aafd
AnasBerkaoui710 <berkaoui.anass12@gmail.com>
2025-10-29 16:15:12 +0100

    10eme challenge

--- 10parking.js
+++ 10parking.js
@@ -1,10 +1,11 @@
 const prompt = require("prompt-sync")()
 
-let table = [1,1,1,0,1,0,1,0,1,0];
+let table = [1,1,1,0,1,1,1,0,1,0];
 
 for(let i=0;i<table.length;i++){
     if(table[i]==0){
         console.log(table.indexOf(table[i]));
         break;
     }
+
 }
